38939aacc842cd659e04091aa2cd76b079f323b7
cheik <cheik.kone@hetic.net>
2020-06-29 23:41:15 +0200

    feat(add layoutPage) add component overlay

---
+++ src/components/layouts/LOverlay.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-show="isActive" class="overlay">
+    <div class="overlay__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isActive: {
+      default: false,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+
+.overlay {
+  background: black;
+  opacity: 0.7;
+  width: 100vw;
+  height: 100vh;
+  position: absolute;
+  z-index: 100;
+  .overlay {
+    z-index: 101;
+  }
+}
+</style>
