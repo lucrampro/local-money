56395d3b77fed8a0eb92d8392a3ef2980b1fd095
cheik <cheik.kone@hetic.net>
2020-06-29 17:26:47 +0200

    feat(add pages inscriptions of companay)

---
+++ src/components/pages/inscription/CompanyContactInformation.vue
@@ -0,0 +1,58 @@
+<template>
+  <ValidationObserver ref="observer">
+    <div class="mt-10">
+      <ValidationProvider name="age" rules="required|min:10|majority" v-slot="{ errors }">
+        <m-input :errors="errors" name="date_of_birth" type="date" v-model="birthDay" maxlength="8" exemple="30/09/1998">Age</m-input>
+      </ValidationProvider>
+      <ValidationProvider name="numéro de téléphone" rules="required|min:3" v-slot="{ errors }">
+        <m-input :errors="errors" name="tel" type="tel" v-model="phoneNumber" mask="## ## ## ## ##" maxlength="14" exemple="06 21 48 37 99">Téléphone</m-input>
+      </ValidationProvider>
+      <ValidationProvider name="mail" rules="email|required" v-slot="{ errors }">
+        <m-input :errors="errors" name="mail" v-model="email" type="mail" exemple="marie.jane@mail.com">
+          Email
+        </m-input>
+      </ValidationProvider>
+      <ValidationProvider name="mot de passe" rules="required|min:6" v-slot="{ errors }">
+          <m-input type="password" name="password" v-model="password" :errors="errors" exemple="*********">
+            Mot de passe
+          </m-input>
+      </ValidationProvider>
+    </div>
+  </ValidationObserver>
+</template>
+<script>
+
+import FormInscriptionPages from './insrcriptionMixin';
+
+export default {
+  name: 'InformationContact',
+  data() {
+    return {
+      birthDay: '',
+      phoneNumber: '',
+      email: '',
+      password: '',
+    };
+  },
+  mixins: [FormInscriptionPages],
+  watch: {
+    model(newVal) {
+      this.$emit('input', newVal);
+    },
+    birthDay(newVal) {
+      this.$emit('updateForm', { birthDay: newVal });
+    },
+    phoneNumber(newVal) {
+      this.$emit('updateForm', { phoneNumber: newVal });
+    },
+    email(newVal) {
+      this.$emit('updateForm', { email: newVal });
+    },
+    password(newVal) {
+      this.$emit('updateForm', { password: newVal });
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ src/components/pages/inscription/CompanyVerificationInformation.vue
@@ -0,0 +1,47 @@
+<template>
+  <ValidationObserver ref="observer">
+    <div class="mt-10">
+      <ValidationProvider name="Ville" rules="required|min:3" v-slot="{ errors }">
+        <m-input :errors="errors" name="city" type="tel" v-model="city" maxlength="30" exemple="Paris">Ville</m-input>
+      </ValidationProvider>
+      <ValidationProvider name="code postal" rules="required|min:3" v-slot="{ errors }">
+        <m-input :errors="errors" name="zipCode" type="tel" v-model="zipCode" maxlength="5" exemple="75000">Code postal</m-input>
+      </ValidationProvider>
+      <ValidationProvider name="address" rules="required|min:6" v-slot="{ errors }">
+        <m-input :errors="errors" name="first_name" v-model="address" exemple="27 Bis Rue du Progrès">Address</m-input>
+      </ValidationProvider>
+      <ValidationProvider name="address" rules="required|min:6" v-slot="{ errors }">
+        <m-input :errors="errors" name="first_name" v-model="address" exemple="27 Bis Rue du Progrès">Address</m-input>
+      </ValidationProvider>
+    </div>
+  </ValidationObserver>
+</template>
+<script>
+
+import FormInscriptionPages from './insrcriptionMixin';
+
+export default {
+  name: 'InformationContact',
+  data() {
+    return {
+      city: '',
+      address: '',
+      zipCode: '',
+    };
+  },
+  mixins: [FormInscriptionPages],
+  watch: {
+    city(newVal) {
+      this.$emit('updateForm', { city: newVal });
+    },
+    address(newVal) {
+      this.$emit('updateForm', { address: newVal });
+    },
+    zipCode(newVal) {
+      this.$emit('updateForm', { zipCode: newVal });
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped></style>
