11e2be1f597dd3285ee67922ae961b1279e38664
cheik <cheik.kone@hetic.net>
2020-06-30 18:56:36 +0200

    feat(switch mode) add switch page and improve component switchButton

--- src/components/pages/my-account/Transaction.vue
+++ src/components/pages/my-account/Transaction.vue
@@ -1,11 +1,34 @@
 <template>
 <div>
-  <a-switch-button />
+  <a-switch-button name="switch__page__transaction" v-model="mode" :leftText="switchButton.leftText" :rightText="switchButton.rightText" />
   <router-view/>
 </div>
 
 </template>
-
+<script>
+export default {
+  data() {
+    return {
+      mode: this.$route.name,
+      switchButton: {
+        leftText: {
+          text: 'Envoyer',
+          value: 'ConvertMoney',
+        },
+        rightText: {
+          text: 'Convertir',
+          value: 'SendMoney',
+        },
+      },
+    };
+  },
+  watch: {
+    mode(newPageName) {
+      this.$router.push({ name: newPageName });
+    },
+  },
+};
+</script>
 <style lang="scss" scoped>
 
 </style>
