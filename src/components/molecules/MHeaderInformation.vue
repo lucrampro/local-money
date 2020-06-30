0a10f2d5263228950969dcd4ad68831cfa997dea
Lucien Ramassamy <lucien.ramassamy@hetic.net>
2020-06-30 15:39:41 +0200

    feat(Home): dev HeaderInformation

---
+++ src/components/molecules/MHeaderInformation.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="header-information">
+    <div class="wrapper--profile-photo">
+      <div class="profile-photo">
+        <p>{{ $props.name[0] }}</p>
+      </div>
+    </div>
+    <div class="welcome-message">
+      <h1>Bonjour <span class="name">{{ $props.name }}</span></h1>
+      <p>Bienvenue sur votre espace 😁 !</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderInformation',
+  props: {
+    name: {
+      type: String,
+      default: 'Elisa',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .header-information {
+    display: flex;
+    align-items: center;
+    .wrapper--profile-photo {
+      margin-right: 10px;
+      height: 70px;
+      width: 70px;
+      border-radius: 50%;
+      border: solid 1px #CECECE;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .profile-photo {
+        background-color: #D16228;
+        border-radius: 50%;
+        height: 90%;
+        width: 90%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        p {
+          color: #fefefe;
+          font-size: 40px;
+          font-weight: normal;
+        }
+      }
+    }
+    .welcome-message {
+      h1 {
+        font-size: 32px;
+        .name {
+          font-weight: bold;
+        }
+      }
+    }
+  }
+</style>
--- src/components/pages/my-account/Home.vue
+++ src/components/pages/my-account/Home.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="home">
-    <m-card-post>
-      <template v-slot:header> BOULANGERIE MARCEL </template>
-      <template v-slot:main> Pour tous les gourmands et gourmandes, aujourd'hui il y a des chouquettes fourrées à la crème chantilly vanillée, profitez en c'est la recette spéciale d'Albert </template>
-    </m-card-post>
+    <HeaderInformation/>
   </div>
 </template>
 
 <script>
-export default {
+import HeaderInformation from '../../molecules/MHeaderInformation.vue';
 
+export default {
+  name: 'Home',
+  components: {
+    HeaderInformation,
+  },
 };
 </script>
-<style>
-
+<style lang="scss" scoped>
+.home {
+  padding: 60px 10px;
+}
 </style>
