1872785e17d3a245c1bd07bd5c50fa81316640b9
cheik <cheik.kone@hetic.net>
2020-06-30 15:32:41 +0200

    feat(carpost hompage) add picto

--- src/components/pages/my-account/Home.vue
+++ src/components/pages/my-account/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <m-card-post>
-      <template v-slot:header> BOULANGERIE MARCEL </template>
+      <template v-slot:header> <a-picto/> BOULANGERIE MARCEL </template>
       <template v-slot:main> Pour tous les gourmands et gourmandes, aujourd'hui il y a des chouquettes fourrées à la crème chantilly vanillée, profitez en c'est la recette spéciale d'Albert </template>
     </m-card-post>
   </div>
@@ -12,6 +12,10 @@ export default {
 
 };
 </script>
-<style>
 
+<style lang="scss" scoped>
+
+::v-deep .picto {
+  margin-right: 10px ;
+}
 </style>
