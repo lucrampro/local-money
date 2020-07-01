4b54443a3fbc8f29faa6c01ccf2c927c9ec1abf2
cheik <cheik.kone@hetic.net>
2020-07-02 00:01:07 +0200

    feat ( Page send post ): init component Send post

---
+++ src/components/pages/my-account/SendPost.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Laisser de mes nouvelles
+    <l-transaction-form>
+      <m-input>Votre message</m-input>
+      <template v-slot:bottom>
+        <button></button>
+      </template>
+    </l-transaction-form>
+  </div>
+</template>
