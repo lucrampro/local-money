2b735b5d08205c1eae67d6a6bc84a813f21a1cab
cheik <cheik.kone@hetic.net>
2020-07-01 13:39:08 +0200

    fix( api ) set good value from back after api call for set in the store

--- src/App.vue
+++ src/App.vue
@@ -48,8 +48,7 @@ export default {
 
     // set infomation of user
     this.$Api.addEventListener('session-user-details', (event) => {
-      console.log(event);
-      this.$store.dispatch('setSolde', event.detail['available_ cash']);
+      this.$store.dispatch('setSolde', event.detail.available_cash);
       this.$store.dispatch('setTransferId', event.detail.account_id);
     });
 
