fc4bb6d0c44c73236633e744dc4758d703c33d22
cheik <cheik.kone@hetic.net>
2020-07-01 16:36:11 +0200

    feat( send and convert money) : improve component and embed component OTransactionInput

--- src/components/pages/my-account/transaction/SendMoney.vue
+++ src/components/pages/my-account/transaction/SendMoney.vue
@@ -1,7 +1,28 @@
 <template>
   <div>
     <l-wrapper-block boxShadow="none" backgroundColor="$gray-background">
-      text
+      <p>Mon solde : {{solde}} MCL</p>
+      <p>Je souhaite faire un envoie d’une valeur de</p>
+      <o-transaction-input />
+      <div class="buttonWrapper">
+        <a-button width="100%">Suivant</a-button>
+      </div>
     </l-wrapper-block>
   </div>
 </template>
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['solde']),
+  },
+};
+</script>
+<style lang="scss" scoped>
+
+.buttonWrapper {
+  padding-top: 90px;
+  padding-bottom: 20px;
+}
+</style>
