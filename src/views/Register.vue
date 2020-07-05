82c950925ba39096305edc0ce2b9dc1eeba899de
Cheik Kone <cheik.kone@numberly.com>
2020-07-06 00:57:05 +0200

    feat( register ): add loader on click submit button

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,7 +2,7 @@
 <ValidationObserver>
   <l-regitster @formSubmit="submitForm">
       <template v-slot:header>
-        <p class="pt-4 text-2xl font-semibold">Bienvenue sur NeyMo !</p>
+        <p class="wellcomeMessage">Bienvenue sur NeyMo !</p>
         <p>Connectez vous pour continuer </p>
       </template>
       <template v-slot:default>
@@ -19,15 +19,15 @@
         </ValidationProvider>
       </div>
         <div class="flex justify-center my-2 mt-8">
-          <a-button  class="w-full" type="submit">Se connecter</a-button>
+          <a-button :onload="formOnload" width="100%" type="submit">Se connecter</a-button>
         </div>
       <div>
-        <a-link class="block mx-auto my-2 mb-4 text-center">
+        <!-- <a-link class="block mx-auto my-2 mb-4 text-center">
           Mot de passe oublié ?
-        </a-link>
+        </a-link> -->
       </div>
         <div class="flex justify-center my-2">
-          <a-button color="#189B73" background="#fefefe"  class="w-full" @click.native="$router.push({ path: 'inscription/account-type' })">S’inscrire</a-button>
+          <a-button color="#189B73" background="#fefefe"  width="100%"  @click.native="$router.push({ path: 'inscription/account-type' })">S’inscrire</a-button>
         </div>
       </template>
   </l-regitster>
@@ -40,16 +40,23 @@ export default {
     return {
       mail: 'particular@neymo.com',
       password: '123456',
+      formOnload: false,
     };
   },
   methods: {
     submitForm() {
+      this.formOnload = true;
+
       this.$Api.login({
         username: this.mail,
         password: this.password,
       }).then(() => {
         this.$router.push({ name: 'Home' });
+        this.formOnload = false;
+      }).catch(() => {
+        this.formOnload = false;
       });
+
       // fonction send data to the back
     },
   },
@@ -57,5 +64,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.wellcomeMessage {
+  font-size: 32px;
+  font-weight: 600;
+}
+::v-deep .Alink {
+  text-align: center;
+  display: block;
+  margin: 20px auto;
+}
 
 </style>
