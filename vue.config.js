f2131215a7a8768f2de4d2f8ee43369bd5e0f5c7
Lucien Ramassamy <lucien.ramassamy@hetic.net>
2020-07-08 02:30:53 +0200

    fix(PWA): add json information

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,36 @@
 module.exports = {
+  pwa: {
+    name: 'PokemonGo-Blog',
+    short_name: 'PokeGo-Blog',
+    themeColor: '#DE4E4B',
+    msTileColor: '#ffffff',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'default',
+    icons: [
+      {
+        src: 'images/icons/icon-72x72.png',
+        sizes: '72x72',
+        type: 'image/png',
+      },
+      {
+        src: 'images/icons/icon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: 'images/icons/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+    ],
+    manifestOptions: {
+      display: 'fullscreen',
+    },
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/service-worker.js',
+    },
+  },
   pluginOptions: {
     i18n: {
       locale: 'fr',
